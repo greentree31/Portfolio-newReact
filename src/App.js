da4f7f50de6e1386616a4475a7b28155d860ff3d
Melisha <melisha.evans06@gmail.com>
2023-09-04 11:02:20 -0700

    update header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React {useState} from 'react';
 import Header from './components/Header';
 import Page from './Page';
 import Footer from './components/Footer';
 
+const pages = ['about','portfolio','contact', 'resume']
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('about');
   return (
     <div className="wrapper">
-      <Header />
+      <Header 
+      pages={pages} 
+      currentPage={currentPage} 
+      setCurrentPage={setCurrentPage} />
       <main>
         <Page />
       </main>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-const Header = () => {
-  return (
-    <header>Header</header>
-  )
+const Header = (props) => {
+  console.log(props);
+  return <header>Header</header>;
 }
 
 export default Header
