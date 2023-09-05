21ac5354c83b190500bbb8c6c0087092ad501d95
Melisha <melisha.evans06@gmail.com>
2023-09-05 14:00:16 -0700

    update about

--- src/Page/About.js
+++ src/Page/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return <>
+  <i className="fas fa-user-circle"></i>
+  <p>Experienced Multimedia Manager with a demonstrated history of working in the plant, mining & metals industry. Skilled in Microsoft Office, Adobe products, e-Learning, SCORM, and web development.</p>
+  <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+
+  </>;
 };
 
 export default About;
